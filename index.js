7ada8fe7c754b466e1a7a9037f37d3ee235e306f
alon1525 <alon1525@gmail.com>
2024-06-08 17:29:35 +0300

    fix

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "alonMan12",
-  port: 5000,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 db.connect();
 
